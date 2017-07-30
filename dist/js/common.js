645f425ab32d731013dd3294e90589f083c8ee10
rdhlb <maxradohleb@gmail.com>
2017-07-30 20:23:18 +0300

    validation (w3c) issues fix

---
+++ dist/js/common.js
@@ -0,0 +1,12 @@
+const changeImgToBackground = (elementId) => {
+	const wrapperTag = document.getElementById(elementId);
+	const img = wrapperTag.getElementsByTagName('img')[0];
+	const src = img.src;
+	img.style.display = 'none';
+	wrapperTag.style.backgroundImage = "url(" + src + ")";
+};
+
+changeImgToBackground('header-photo');
+changeImgToBackground('footer-photo');
+//changeImgToBackground('project-1');
+
