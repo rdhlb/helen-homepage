8881ac322d8945db3e00f09c1c8e3b284899f8a7
rdhlb <maxradohleb@gmail.com>
2017-08-01 17:30:06 +0300

    portrait-photo was added to first slide

--- dist/js/common.js
+++ dist/js/common.js
@@ -9,6 +9,36 @@ const changeImgToBackground = (elementId) => {
 changeImgToBackground('header-photo');
 changeImgToBackground('footer-photo');
 
+
+// mmenu initialization: 
+var $menu = $("#my-menu").mmenu({
+  extensions: ['theme-dark', 'border-none', 'listview-huge'],
+  navbar: {
+			title: 'Menu'
+		}
+});
+
+var $icon = $("#hamburger");
+var API = $menu.data( "mmenu" );
+
+$icon.on( "click", function() {
+   API.open();
+});
+
+API.bind( "open:finish", function() {
+   setTimeout(function() {
+      $icon.addClass( "is-active" );
+   }, 10);
+});
+
+API.bind( "close:finish", function() {
+   setTimeout(function() {
+      $icon.removeClass( "is-active" );
+   }, 10);
+});
+
+
+// fullpage initialization:
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		verticalCentered: false
