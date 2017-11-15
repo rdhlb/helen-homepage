84131df1c3f22d17855160adeff33942fdc81869
rdhlb <maxradohleb@gmail.com>
2017-11-15 21:56:25 +0200

    refactor media queries

--- app/js/common.js
+++ app/js/common.js
@@ -1,26 +1,26 @@
 // mmenu initialization: 
-// var $menu = $("#my-menu").mmenu({
-//   extensions: ['theme-dark', 'border-none', 'listview-huge'],
-//   navbar: {
-// 			title: 'Menu'
-// 		}
-// });
+var $menu = $("#my-menu").mmenu({
+  extensions: ['theme-dark', 'border-none', 'listview-huge'],
+  navbar: {
+			title: 'Menu'
+		}
+});
 
-// var $icon = $("#hamburger");
-// var API = $menu.data( "mmenu" );
+var $icon = $("#hamburger");
+var API = $menu.data( "mmenu" );
 
-// $icon.on( "click", function() {
-//    API.open();
-// });
+$icon.on( "click", function() {
+   API.open();
+});
 
-// API.bind( "open:finish", function() {
-//    setTimeout(function() {
-//       $icon.addClass( "is-active" );
-//    }, 10);
-// });
+API.bind( "open:finish", function() {
+   setTimeout(function() {
+      $icon.addClass( "is-active" );
+   }, 10);
+});
 
-// API.bind( "close:finish", function() {
-//    setTimeout(function() {
-//       $icon.removeClass( "is-active" );
-//    }, 10);
-// });
+API.bind( "close:finish", function() {
+   setTimeout(function() {
+      $icon.removeClass( "is-active" );
+   }, 10);
+});
