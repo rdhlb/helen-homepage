2c953471a1e16ad7a60cea72daa78bf0361291b3
rdhlb <maxradohleb@gmail.com>
2017-11-15 18:24:32 +0200

    temporary remove mobile version

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+// TODO: выполнять только после проверки CSS-support'ом
 const changeImgToBackground = (elementId) => {
 	const wrapperTag = document.getElementById(elementId);
 	const img = wrapperTag.getElementsByTagName('img')[0];
@@ -10,28 +11,28 @@ changeImgToBackground('header-photo');
 changeImgToBackground('footer-photo');
 
 // mmenu initialization: 
-var $menu = $("#my-menu").mmenu({
-  extensions: ['theme-dark', 'border-none', 'listview-huge'],
-  navbar: {
-			title: 'Menu'
-		}
-});
+// var $menu = $("#my-menu").mmenu({
+//   extensions: ['theme-dark', 'border-none', 'listview-huge'],
+//   navbar: {
+// 			title: 'Menu'
+// 		}
+// });
 
-var $icon = $("#hamburger");
-var API = $menu.data( "mmenu" );
+// var $icon = $("#hamburger");
+// var API = $menu.data( "mmenu" );
 
-$icon.on( "click", function() {
-   API.open();
-});
+// $icon.on( "click", function() {
+//    API.open();
+// });
 
-API.bind( "open:finish", function() {
-   setTimeout(function() {
-      $icon.addClass( "is-active" );
-   }, 10);
-});
+// API.bind( "open:finish", function() {
+//    setTimeout(function() {
+//       $icon.addClass( "is-active" );
+//    }, 10);
+// });
 
-API.bind( "close:finish", function() {
-   setTimeout(function() {
-      $icon.removeClass( "is-active" );
-   }, 10);
-});
+// API.bind( "close:finish", function() {
+//    setTimeout(function() {
+//       $icon.removeClass( "is-active" );
+//    }, 10);
+// });
