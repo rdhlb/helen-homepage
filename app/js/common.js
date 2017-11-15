bcdf09f600f678857da83d3bdd660ca4e6ad16da
rdhlb <maxradohleb@gmail.com>
2017-11-15 19:19:42 +0200

    remove inline-img to bcg-img transform script (because of object-fit property)

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,3 @@
-// TODO: выполнять только после проверки CSS-support'ом
-const changeImgToBackground = (elementId) => {
-	const wrapperTag = document.getElementById(elementId);
-	const img = wrapperTag.getElementsByTagName('img')[0];
-	const src = img.src;
-	img.style.display = 'none';
-	wrapperTag.style.backgroundImage = "url(" + src + ")";
-};
-
-changeImgToBackground('header-photo');
-changeImgToBackground('footer-photo');
-
 // mmenu initialization: 
 // var $menu = $("#my-menu").mmenu({
 //   extensions: ['theme-dark', 'border-none', 'listview-huge'],
