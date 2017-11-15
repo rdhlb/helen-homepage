677e69ae897b158526e48831a8b205be6436e119
rdhlb <maxradohleb@gmail.com>
2017-11-16 00:12:20 +0200

    return fullpage for screen size >1024

--- app/js/common.js
+++ app/js/common.js
@@ -1,26 +1,36 @@
 // mmenu initialization: 
 var $menu = $("#my-menu").mmenu({
-  extensions: ['theme-dark', 'border-none', 'listview-huge'],
-  navbar: {
-			title: 'Menu'
-		}
+	extensions: ['theme-dark', 'border-none', 'listview-huge'],
+	navbar: {
+		title: 'Menu'
+	}
 });
 
 var $icon = $("#hamburger");
 var API = $menu.data( "mmenu" );
 
 $icon.on( "click", function() {
-   API.open();
+	API.open();
 });
 
 API.bind( "open:finish", function() {
-   setTimeout(function() {
-      $icon.addClass( "is-active" );
-   }, 10);
+	setTimeout(function() {
+		$icon.addClass( "is-active" );
+	}, 10);
 });
 
 API.bind( "close:finish", function() {
-   setTimeout(function() {
-      $icon.removeClass( "is-active" );
-   }, 10);
+	setTimeout(function() {
+		$icon.removeClass( "is-active" );
+	}, 10);
+});
+
+$(document).ready(function() {
+	if(screen.width > 1024) {
+		//mobile            
+		$('#fullpage').fullpage({
+			verticalCentered: false,
+			loopBottom: true,
+		});
+	}
 });
